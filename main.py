952b7592432e1093626b57beec357e9c6a24ad42
masharo <janemasharo@gmail.com>
2025-10-24 23:49:21 +0300

    function for fiilind ships and enemy matrix

--- main.py
+++ main.py
@@ -106,6 +106,96 @@ def filling_ship_ver(matrix, number_y_first, number_y_second, number_x_first):
             matrix[number_y][number_x_first] = 1
     return matrix
 
+def enemy_card(enemy_matrix, matrix, number_y, number_x):
+    match matrix[number_y][number_x]:
+        case '-':
+            enemy_matrix[number_y][number_x] = 'O'
+        case 'X':
+            enemy_matrix[number_y][number_x] = 'X'
+        case 'R':
+            enemy_matrix[number_y][number_x] = 'R'
+    for row in enemy_matrix:
+        print(row)
+    return enemy_matrix
+
+
+def filling_ship_hor(matrix, number_y_first, number_x_first, number_x_second):
+    if number_x_first < number_x_second:
+        for number_x in range(number_x_first, number_x_second + 1):
+            if not matrix[number_y_first][number_x]:
+                matrix[number_y_first][number_x] = '1'
+    else:
+        for number_x in range(number_x_second, number_x_first + 1):
+            matrix[number_y_first][number_x] = '1'
+    return matrix
+
+
+def filling_ship_ver(matrix, number_y_first, number_y_second, number_x_first):
+    if number_y_first < number_y_second:
+        for number_y in range(number_y_first, number_y_second + 1):
+            matrix[number_y][number_x_first] = '1'
+    else:
+        for number_y in range(number_y_second, number_y_first + 1):
+            matrix[number_y][number_x_first] = '1'
+    return matrix
+
+
+
+def cells_ship(matrix, number_y_first, number_x_first, number_y_second, number_x_second):
+    is_correct_coord = True
+    start_x = 0
+    start_y = 0
+    stop_y = 0
+    if number_y_first | number_y_second | number_x_first | number_x_second not in range(0, 10):
+        is_correct_coord = False
+    else:
+        if number_y_first == number_y_second:
+            cells_count = abs(number_x_first - number_x_second)
+            if number_y_first == 0:
+                start_y = 0
+                stop_y = 2
+            elif number_y_first == 9:
+                start_y = 8
+                stop_y = 10
+            else:
+                start_y = number_y_first - 1
+                stop_y = number_y_first + 2
+            if number_x_first <= number_x_second:
+                start_x = number_x_first
+            else:
+                start_x = number_x_second
+            for i in range(start_y, stop_y):
+                    for j in range(start_x, start_x + cells_count):
+                        if matrix[i][j] == '1':
+                            is_correct_coord = False
+                        else:
+                            filling_ship_hor(matrix, number_y_first, number_x_first, number_x_second)
+        elif number_x_first == number_x_second:
+            cells_count = abs(number_y_first - number_y_second)
+            if number_x_first == 0:
+                start_x = 0
+                stop_x = 2
+            elif number_x_first == 9:
+                start_x = 8
+                stop_x = 10
+            else:
+                start_x = number_x_first - 1
+                stop_x = number_x_first + 2
+            if number_y_first <= number_y_second:
+                start_y = number_y_first
+            else:
+                start_y = number_y_second
+            for i in range(start_x, stop_x):
+                    for j in range(start_y, start_y + cells_count):
+                        if matrix[j][i] == '1':
+                            is_correct_coord = False
+                        else:
+                            filling_ship_ver(matrix, number_y_first, number_y_second, number_x_first)
+        else:
+            is_correct_coord = False
+    return is_correct_coord, matrix
+
+
 player1_matrix = create_matrix()
 player2_matrix = create_matrix()
 
