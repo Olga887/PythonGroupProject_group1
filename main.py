88c1d9e2bdb9122872221b6dd388385ccd82d917
Dmitrii.Korepin <dmitrii.korepin@zumtobelgroup.com>
2025-10-24 19:15:15 +0200

    "Matrix"

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-print("refactor commit")
-print("Hello World")
-print("Hello from Dmitrii")
+def create_matrix():
+    return [['-' for _ in range(10)] for _ in range(10)]
+
+player1_matrix = create_matrix()
+player2_matrix = create_matrix()
+
+print("Player 1 matrix:")
+for row in player1_matrix:
+    print(" ".join(row))
+
+print("\nPlayer 2 matrix:")
+for row in player2_matrix:
+    print(" ".join(row))
 print("Hello from Yuliya")
+
