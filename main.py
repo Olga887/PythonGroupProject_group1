bed757ef63397a69973c3fa50fd93b384863f449
Dmitrii.Korepin <dmitrii.korepin@zumtobelgroup.com>
2025-10-24 19:27:30 +0200

    "Matrix 3"

--- main.py
+++ main.py
@@ -15,3 +15,20 @@ print_matrix_with_coords(player1_matrix)
 
 print("\nInitial matrix player 2:")
 print_matrix_with_coords(player2_matrix)
+
+
+def parse_coordinates(coord):
+    if len(coord) < 2 or len(coord) > 3:
+        raise ValueError("Wrong format. Example: B5 or J0")
+
+    row_letter = coord[0].upper()
+    col_number = coord[1:]
+
+    if not row_letter.isalpha() or not col_number.isdigit():
+        raise ValueError("Coord should have a letter and nuber")
+
+    row_index = ord(row_letter) - ord('A')
+    col_index = int(col_number)
+
+    if not (0 <= row_index < 10 and 0 <= col_index < 10):
+        raise ValueError("Out of acceptable range (A–J, 0–9)")
